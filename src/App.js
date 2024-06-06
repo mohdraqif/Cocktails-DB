5ec6692497a42adc1c5383fdd3961e029fad829b
mohdraqif <48005159+mohdraqif@users.noreply.github.com>
2024-06-06 11:43:44 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import Error from './pages/Error'
+import Home from './pages/Home'
+import About from './pages/About'
+import Footer from './components/Footer'
+import SingleCocktail from './pages/SingleCocktail'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' Component={Home}></Route>
+        <Route exact path='/about' Component={About}></Route>
+        <Route exact path='/cocktail/:id' Component={SingleCocktail}></Route>
+        <Route component={Error}></Route>
+      </Routes>
+      <Footer />
+    </React.Fragment>
   );
 }
 
